d0d01c1400d44c009e148323744e76ce0a1d5bd6
Harlan Cleiton <harlancleiton@gmail.com>
2022-01-12 19:54:20 -0300

    chore(users): add PrismaUserRepository

---
+++ app/modules/users/infra/prisma/repositories/PrismaUserRepository.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from '@prisma/client';
+
+import { CreateUserInput, UserRepository } from '~/modules/users/data';
+import { UserModel } from '~/modules/users/domain';
+
+export class PrismaUserRepository implements UserRepository {
+  constructor(private readonly prismaClient: PrismaClient) {}
+
+  public create(input: CreateUserInput): Promise<UserModel> {
+    return this.prismaClient.user.create({ data: input });
+  }
+}
