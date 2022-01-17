1929754c921f73448fdb93da4cc1add69b1fa804
Harlan Cleiton <harlancleiton@gmail.com>
2022-01-17 08:49:29 -0300

    chore(movements/presentation): add AddMovementController

---
+++ app/modules/movements/presentation/controllers/add-movement.controller.ts
@@ -0,0 +1,26 @@
+import {
+  ControllerContract,
+  HttpContextContract,
+  RequestValidatorContract
+} from '~/common';
+
+import { AddMovement } from '../../domain';
+
+export class AddMovementController implements ControllerContract {
+  constructor(
+    private readonly addMovement: AddMovement,
+    private readonly validation: RequestValidatorContract
+  ) {}
+
+  public async handle({
+    request,
+    response
+  }: HttpContextContract): Promise<void> {
+    const { errors, payload } = await this.validation.validate(request);
+
+    if (errors.length) return response.badRequest({ errors });
+
+    const movement = await this.addMovement.execute(payload);
+    response.created(movement);
+  }
+}
